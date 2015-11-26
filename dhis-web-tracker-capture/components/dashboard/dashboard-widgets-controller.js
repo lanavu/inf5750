906b94ad615ac74189c3974bba220e74fd11ba8e
Inthu <inthura@ifi.uio.no>
2015-11-26 14:58:33 +0100

    Removed test-version. Starting for real soooooon

--- dhis-web-tracker-capture/components/dashboard/dashboard-widgets-controller.js
+++
@@ -1,9 +0,0 @@
-//Controller for the dashboard widgets
-trackerCapture.controller('DashboardWidgetsController', 
-    function($scope, 
-            $modalInstance){
-    
-    $scope.close = function () {
-        $modalInstance.close();
-    };       
-});
